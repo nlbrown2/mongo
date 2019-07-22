bd9eb71786b199c816e099121e3249a44d66a349
Alya Berciu <alya.berciu@mongodb.com>
2019-07-22 16:09:39 -0400

    Python refactorings

--- src/mongo/platform/usdt/python_tester/tester/generator.py
+++ src/mongo/platform/usdt/python_tester/tester/generator.py
@@ -1,12 +1,8 @@
 """ module that can generate c-style eBPF programs given a list of probes to attach to and their arguments """
-from .util import STRING_TYPE, STRUCT_TYPE, PROBE_NAME_KEY, PROBE_ARGS_KEY, ARG_TYPE_KEY, POINTER_TYPE
-
-PROBE_HIT_KEY = "hits"
-ARG_STR_LEN_KEY = "length"
-ARG_STRUCT_FIELDS_KEY = "fields"
+from .util import *
 
 class Probe:
-    """ representation of a probe specification useful for code generation """
+    """ Representation of a probe specification useful for code generation. """
     def __init__(self, probe_dict):
         assert isinstance(probe_dict, dict)
 
@@ -16,16 +12,34 @@ class Probe:
         self.hits = probe_dict[PROBE_HIT_KEY]
         assert isinstance(self.hits, int)
 
-        self.args = [Arg(arg, self.hits, self.name, index) for index, arg in enumerate(probe_dict[PROBE_ARGS_KEY])]
-        self.function_name = self.name + '_fn'
+        self.args = [Arg(arg, self.hits, self.name, index)
+                        for index, arg in enumerate(probe_dict[PROBE_ARGS_KEY])]
+
+        self.function_name = PROBE_FN_NAME.format(self.name)
+        self.output_struct_name = BPF_PERF_OUTPUT_STRUCT_NAME.format(self.name)
+
+    def before_output_gen(self):
+        return reduce(Arg.before_output_gen, self.args)
+
+    def bpf_perf_output_gen(self):
+        c_prog = BPF_PERF_OUTPUT.format(self.name)
+        c_prog += STRUCT.format(self.output_struct_name,
+                                reduce(Arg.get_output_struct_def, self.args))
+        return c_prog
+
+    def entry_fn_gen(self):
+        fn_content = STRUCT_INIT.format(self.output_struct_name, "out")
+        fn_content += reduce(Arg.fill_output_struct, self.args)
+        fn_content += BPF_PERF_SUBMIT_STMT.format(self.name)
+        return PROBE_ENTRY_FN.format(self.function_name, fn_content)
 
 class Arg:
-    """ Representation of an argument to a Probe holding information about where it can be located """
+    """ Representation of an argument to a Probe holding information about where it can be located. """
     def __init__(self, arg_dict, num_hits, probe_name, index, depth=0):
         assert isinstance(arg_dict, dict)
 
         self.type = arg_dict[ARG_TYPE_KEY]
-        assert self.type in (STRING_TYPE, STRUCT_TYPE, 'int', POINTER_TYPE)
+        assert self.type in (STRING_TYPE, STRUCT_TYPE, INT_TYPE, POINTER_TYPE)
 
         self.probe_name = probe_name
         assert isinstance(self.probe_name, str)
@@ -36,55 +50,62 @@ class Arg:
         self.index = index
         assert isinstance(self.index, int)
 
-        self.output_arg_name = 'arg_{}_{}'.format(self.depth, self.index)
+        self.output_arg_name = BPF_PERF_OUTPUT_ARG_NAME.format(self.depth, self.index)
+        self.output_addr_name = BPF_PERF_OUTPUT_ADDR_NAME.format(self.depth, self.index)
 
         if self.type == STRING_TYPE:
             self.length = arg_dict[ARG_STR_LEN_KEY]
             assert isinstance(self.length, int)
+        else:
+            self.length = 0
 
         if self.type == STRUCT_TYPE:
-            self.fields = [Arg(val, num_hits, probe_name, child_index, depth=depth+1) for child_index, val in enumerate(arg_dict[ARG_STRUCT_FIELDS_KEY])]
-            for field in self.fields:
-                field.output_arg_name += '_{}'.format(self.index)
+            self.output_struct_name = STRUCT_NAME.format(arg_name = self.output_arg_name,
+                                                         probe_name = self.probe_name,
+                                                         depth = self.depth,
+                                                         index = self.index)
 
-    def get_c_name(self):
-        """ return the type and name of this argument in a C program. The name should be unique to an instance but the same across instances """
-        if self.type == STRING_TYPE:
-            return 'char {arg_name}[{length}]'.format(arg_name=self.output_arg_name, length=self.length)
-        elif self.type == STRUCT_TYPE:
-            return 'struct {probe_name}_level_{depth}_{index} {arg_name}'.format(probe_name=self.probe_name, depth=self.depth, arg_name=self.output_arg_name, index=self.index)
-        elif self.type == POINTER_TYPE:
-            return 'void* {arg_name}'.format(arg_name=self.output_arg_name)
-        else:
-            return '{type_} {arg_name}'.format(type_=self.type, arg_name=self.output_arg_name)
+            self.fields = [Arg(val, num_hits, probe_name, child_index, depth=depth+1)
+                            for child_index, val in enumerate(arg_dict[ARG_STRUCT_FIELDS_KEY])]
+
+            for field in self.fields:
+                field.output_arg_name += ARG_NAME_CAT.format(self.index)
+                field.output_addr_name += ARG_NAME_CAT.format(self.index)
+
+    def get_c_decl(self):
+        """ Returns the type and name of this argument in a C program.
+            The name should be unique to an instance but the same across instances. """
+        return declare(arg_type=self.type,
+                       probe_name = self.probe_name,
+                       arg_name = self.output_arg_name,
+                       depth = self.depth,
+                       index = self.index,
+                       length = self.length)
 
     def before_output_gen(self):
         """ Returns a string of any code that needs to be emitted before the output struct containing this argument is emitted.
-            This allows structs to print their definitions (any any nested definitions) before they are referenced."""
+            This allows structs to print their definitions (any any nested definitions) before they are referenced. """
         if self.type != STRUCT_TYPE:
             return ''
+
+        members = ''
         result = ''
         for member in self.fields:
-            if member.type == STRUCT_TYPE:
-                result += member.before_output_gen()
+            result += member.before_output_gen()
+            members += STRUCT_MEMBER.format(member.get_c_decl())
+        result += STRUCT.format(self.output_struct_name, members)
 
-        result += 'struct {probe_name}_level_{depth}_{index} {{\n'.format(probe_name=self.probe_name, depth=self.depth, index=self.index)
-        for member in self.fields:
-            result += '\t' + member.get_c_name() + ';\n'
-        result += '};\n'
         return result
 
     def get_output_struct_def(self):
-        """ returns what members in the output struct this arg is responsible for. """
+        """ Returns what members in the output struct this arg is responsible for. """
         if self.type != STRUCT_TYPE:
-            return '\t' + self.get_c_name() + ';\n'
-        result = ''
-        for arg in self.fields:
-            result += arg.get_output_struct_def()
-        return result
+            return STRUCT_MEMBER.format(self.get_c_decl())
+
+        return reduce(Arg.get_output_struct_def, self.fields)
 
     def fill_output_struct(self, source_struct_name=None):
-        """ returns the code necessary to fill the members of the output struct this arg is responsible for """
+        """ Returns the code necessary to fill the members of the output struct this arg is responsible for. """
         if source_struct_name:
             # we should be reading our value out of a struct
             if self.type == STRING_TYPE:
@@ -92,72 +113,65 @@ class Arg:
                 # Thus, to copy strings from embedded structs to the output struct, the offset within the passed in struct
                 # is determined, and then a bpf_probe_read_str is issued, reading the string from userspace once more
                 # See https://github.com/iovisor/bcc/issues/691 for updates on string builtin functions
-                result = """
-\tconst char* {str_addr_name} = {source};
-\tbpf_probe_read_str(out.{target}, sizeof(out.{target}), {str_addr_name});
-""".format(source=source_struct_name + '.' + self.output_arg_name,
-                        target=self.output_arg_name,
-                        str_addr_name=self.output_arg_name.replace('arg', 'addr'))
+                result = BPF_READ_STRUCT_MEMBER_STR.format(
+                            source = MEMBER_GET_CAT.format(source_struct_name, self.output_arg_name),
+                            target = self.output_arg_name,
+                            str_addr_name = self.output_addr_name)
                 return result
+
             elif self.type == STRUCT_TYPE:
                 # add ourselves to the source struct name for our fields to read out of
                 result = ''
-                source_struct_name += '.' + self.output_arg_name
+                source_struct_name = MEMBER_GET_CAT.format(source_struct_name, self.output_arg_name)
                 for arg in self.fields:
                     result += arg.fill_output_struct(source_struct_name) + '\n'
+
                 return result
-            return '\tout.{target} = {source};\n'.format(target=self.output_arg_name, source=source_struct_name + '.' + self.output_arg_name)
+
+            return BPF_PERF_OUTPUT_MEMBER_ASSN.format(
+                        target=self.output_arg_name,
+                        source_struct=source_struct_name,
+                        source_struct_member=self.output_arg_name)
+
         elif self.type == STRING_TYPE:
             assert self.depth == 0
             # read the addr out of the USDT arg and read the C-string into our output struct
-            return """
-\tconst char* {addr_name} = NULL;
-\tbpf_usdt_readarg({arg_num}, ctx, &{addr_name});
-\tbpf_probe_read_str(&out.{out_member}, sizeof(out.{out_member}), {addr_name});
-""".format(arg_num=self.index + 1, addr_name=self.output_arg_name.replace('arg', 'addr'), out_member=self.output_arg_name)
+            return BPF_READ_STR.format(
+                        probe_name=self.probe_name,
+                        arg_num=self.index + 1,
+                        addr_name=self.output_addr_name,
+                        out_member=self.output_arg_name)
+
         elif self.type == STRUCT_TYPE:
             assert self.depth == 0
             # read in the struct from the USDT arg pointer
-            struct_name = '{probe_name}_level_0_{index}_base'.format(probe_name=self.probe_name, index=self.index)
-            result = """
-\tstruct {probe_name}_level_0_{index} {struct_name} = {{}};
-\tconst void* addr_{index} = NULL;
-\tbpf_usdt_readarg({arg_num}, ctx, &addr_{index});
-\tbpf_probe_read(&{struct_name}, sizeof({struct_name}), addr_{index});
-""".format(probe_name=self.probe_name, index=self.index, struct_name=struct_name, arg_num=self.index + 1)
+            struct_name = BASE_STRUCT_NAME.format(probe_name=self.probe_name, index=self.index)
+            result = BPF_READ_STRUCT.format(probe_name=self.probe_name,
+                                            index=self.index,
+                                            struct_name=struct_name,
+                                            arg_num=self.index + 1)
+
             for arg in self.fields:
                 result += arg.fill_output_struct(struct_name) + '\n'
             return result
+
         else:
             # read the argument directly
-            return '\tbpf_usdt_readarg({}, ctx, &out.{});\n'.format(self.index + 1, self.output_arg_name)
+            return BPF_READ_ARG.format(num=self.index + 1, output_member_name=self.output_arg_name)
 
 class Generator:
-    """ Responsible for orchestrating the generation of code for each probe that gets added to it """
+    """ Responsible for orchestrating the generation of code for each probe that gets added to it. """
     def __init__(self):
-        self.c_prog = '#include <linux/ptrace.h>\n'
+        self.c_prog = HEADERS
 
     def finish(self):
-        """ Do any clean up work and then provide the generated C program """
+        """ Do any clean up work and then provide the generated C program. """
         return self.c_prog
 
     def add_probe(self, probe):
-        """ add a probe and generate code to attach that probe to its own output channel and function """
-        if not isinstance(probe, Probe):
-            raise TypeError("Probe must be a dict!")
-        for arg in probe.args:
-            if arg.type == STRUCT_TYPE:
-                self.c_prog += arg.before_output_gen()
+        """ Add a probe and generate code to attach that probe to its own output channel and function. """
+        assert isinstance(probe, Probe)
 
-        self.c_prog += "\n\nBPF_PERF_OUTPUT({});\n".format(probe.name)
-        self.c_prog += "\nstruct {}_output {{\n".format(probe.name)
-        for  arg in probe.args:
-            self.c_prog += arg.get_output_struct_def()
-        self.c_prog += '};\n\n'
-
-        self.c_prog += 'int {}(struct pt_regs *ctx) {{\n'.format(probe.function_name)
-        self.c_prog += '\tstruct {}_output out = {{}};\n'.format(probe.name)
-        for arg in probe.args:
-            self.c_prog += arg.fill_output_struct()
-        self.c_prog += '\t{}.perf_submit(ctx, &out, sizeof(out));\n'.format(probe.name)
-        self.c_prog += '\treturn 0;\n}\n'
+        self.c_prog += probe.before_output_gen()
+        self.c_prog += probe.bpf_perf_output_gen()
+        self.c_prog += probe.entry_fn_gen()
